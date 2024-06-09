aee25f5da1eba53e4d04eebee95e81d3a0a69671
rapsingh <67787691+rapsingh@users.noreply.github.com>
2024-06-09 14:38:38 +0530

    changes

--- 9.dictionary/dictionary.py
+++ 9.dictionary/dictionary.py
@@ -1,2 +1,18 @@
-chai_types = {'Masala':'spicy','Ginger':'zesty','Green':'Mild'}
+chai_types = {'Masala':'Spicy','Ginger':'Zesty','Green':'Mild'}
 print(chai_types)
+print(chai_types['Masala'])
+print(chai_types.get('Ginger'))
+chai_types['Ginger']='Fresh'
+print(chai_types)
+for chai in chai_types:
+    print(chai)
+for chai in chai_types:
+    print(chai,chai_types[chai])
+for key,value in chai_types.items():
+    print(key,value)
+if 'Masala' in chai_types:
+    print("Yes Masala is present in chai_types")
+print(len(chai_types))
+chai_types["Earl Grey"]='Exotic'
+print(chai_types)
+print(len(chai_types))
