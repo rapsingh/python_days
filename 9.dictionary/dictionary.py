02ca52c1ffc4ea6187b7e37c4e8704ac98f7a56b
rapsingh <67787691+rapsingh@users.noreply.github.com>
2024-06-09 15:44:54 +0530

    tuple changes

--- 9.dictionary/dictionary.py
+++ 9.dictionary/dictionary.py
@@ -16,3 +16,19 @@ print(len(chai_types))
 chai_types["Earl Grey"]='Exotic'
 print(chai_types)
 print(len(chai_types))
+print(chai_types.pop('Ginger'))
+print(chai_types)
+print(chai_types.popitem())
+print(chai_types)#earl gray removed because it was lastly added
+del(chai_types['Green'])
+print(chai_types)
+chai_types_copy=chai_types.copy()
+chai_types.popitem()
+print(chai_types)
+print(chai_types_copy)
+tea_shop = { 'chai': {'Masala':'Spicy','Ginger':'Zesty'},'tea':{'Black':'Strong','Green':'Mild'}}
+print(tea_shop)
+print(tea_shop['chai'])
+print(tea_shop['chai']['Masala'])
+squared_num = {x:x**3 for x in range(9)}
+print(squared_num)
